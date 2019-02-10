54e6e505f03f98f25f3acaa06e5982c44b848f68
anthonybo <twandeez@gmail.com>
2019-02-09 19:14:55 -0800

    Anthony Bocchino - PROTOTYPE - React - Intro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber () {
+    var randomNum = Math.floor(Math.random() * 1000) + 1;
+    return randomNum;
+}
+
+function greeting (user) {
+    return (
+        <div className='container'>
+            <h1>Hello there {user.name}</h1>
+            <h2 className='text-muted'>Your lucky number is {user.luckyNumber}</h2>
+        </div>
+    );
+}
+
+const user = {
+    name: 'Bob',
+    luckyNumber: luckyNumber()
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
