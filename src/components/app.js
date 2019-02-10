f05c09f32253f2042162bf3c491e668f467b5977
anthonybo <twandeez@gmail.com>
2019-02-09 19:54:58 -0800

    Anthony Bocchino - React Functional Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,29 @@
+import React from 'react';
+import Table from './table'
 
+const students = [
+    {
+        name: 'Paul D.',
+        course: 'Databases',
+        grade: 62
+    },
+    {
+        name: 'Tom F.',
+        course: 'Math',
+        grade: 75
+    },
+    {
+        name: 'Randell C.',
+        course: 'Science',
+        grade: 89
+    }
+];
 
+export default () => {
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    );
+}
